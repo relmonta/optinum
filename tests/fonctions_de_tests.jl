9974f18515f249e5247b5a4caf1c6b661bc4adc9
Mohamed <hindahwakhfe@gmail.com>
2020-06-04 10:53:47 +0200

    fonctions de tests données dans le sujet

--- tests/fonctions_de_tests.jl
+++ tests/fonctions_de_tests.jl
@@ -1,3 +1,10 @@
+#function f1
+function f(x)
+
+y =  2*(x[1]+x[2]+x[3]-3)^2 + (x[1]-x[2])^2 + (x[2]-x[3])^2
+return [y]
+end
+
 #la gradient de la fonction f1
 function gradf1(x)
 
@@ -6,5 +13,13 @@ y2 = 4*(x[1]+x[2]+x[3]-3) - 2*(x[1]-x[2]) +2*(x[2]-x[3])
 y3 = 4*(x[1]+x[2]+x[3]-3) - 2*(x[2]-x[3])
 
 y = [y1 y2 y3]'
+return y
+end
+
+#l'hessienne de la fonction f1
+function hessf1(x)
+
+y = [6 2 4;2 8 2;4 2 6]
+
 return y
 end
--- tests/hessf1.jl
+++
@@ -1,7 +0,0 @@
-#l'hessienne de la fonction f1
-function hessf1(x)
-
-y = [6 2 4;2 8 2;4 2 6]
-
-return y
-end
