a0b15287498fb17ec47d2097b818f21dd2cf51e3
Mohamed <hindahwakhfe@gmail.com>
2020-06-05 10:26:25 +0200

    tests à compléter du Lagrangien Augmenté

--- src/Lagrangien_Augm.jl
+++ src/Lagrangien_Augm.jl
@@ -1,3 +1,4 @@
+"""
 #################################################################################
 	# Algorithme du lagrangien augmenté pour le
 	# calcul du minimum d'une fonction avec des contraintes d'égalités
@@ -5,36 +6,43 @@
 
 #################################################################################
 #Entrées :
-	#l'indice inqiquant l'algorithme sans contraintes à utiliser : algorithme_sans_contrainte
-	#		0 : pour utiliser Newton
-	#		1 : pour utiliser RC
-	#la fonction à minimiser : fonc
-	#la contrainte : contrainte [x est dans le domaine des contraintes ssi c(x)==0]
-	#le gradient de la fonction : gradfonc
-	#la hessienne de la fonction : hessfonc
-	#le gradient de la contrainte contrainte : gradcontrainte
-	#la hessienne de la contrainte : hesscontrainte
-	#la norme de la contrainte : nomrcontrainte
-	#le jacobien de la contrainte : jaccontrainte
-	#le point du départ : x0
-	#variables initiales de l'algorithme : lambda0,mu0,tho
+	# algorithme_sans_contrainte : l'indice inqiquant l'algorithme sans contraintes à utiliser
+	#		"NW" : pour utiliser l'algo de Newton
+	#		"RC" : pour utiliser l'algo de Région de confiance
+	# fonc : la fonction à minimiser 
+	# contrainte : la contrainte [x est dans le domaine des contraintes ssi c(x)==0]
+	# gradfonc : le gradient de la fonction
+	# hessfonc : la hessienne de la fonction 
+	# gradcontrainte : le gradient de la contrainte contrainte
+	# hesscontrainte : la hessienne de la contrainte
+	# nomrcontrainte : la norme de la contrainte
+	# jaccontrainte : le jacobien de la contrainte
+	# x0 : le point du départ
+	# epsilon : utilisé dans les critéres d'arrêt
+	# tol : utilisé dans les critéres d'arrêt
+	# itermax : nombre maximal d'itération dans la boucle principale
+	# lambda0,mu0,tho : valeurs initiales des variables de l'algorithme
 
 #################################################################################
 
 #################################################################################
 #Sorties :
 	#xmin :Le minumum du problème avec contraintes
+	#fxmin : l'image de xmin par la fonction objectif
+	#niters : nombre d'itérations
+	#flag : indicateur du déroulement de l'algorithme
+	#	0 : Convergence
+	#	1 : nombre maximal d'itération atteint
+	#	-1: une erreur s'est produite
 #################################################################################
 
+"""
+function Lagrangien_Augm(algorithme_sans_contrainte,fonc::Function,contrainte::Function,gradfonc::Function,hessfonc::Function,gradcontrainte::Function,hesscontrainte::Function,normcontrainte::Function,jaccontrainte::Function,phi::Function,x0,epsilon,tol,itermax,lambda0,mu0,tho)
 
-function Lagrangien_Augm(algorithme_sans_contrainte,fonc::Function,contrainte::Function,gradfonc::Function,hessfonc::Function,gradcontrainte::Function,hesscontrainte::Function,normcontrainte::Function,jaccontrainte::Function,phi::Function,x0,lambda0,mu0,tho)
 
-#paramétres utiles pour définir les critéres d'arrêts
-epsilon = 1e-30
-tol = 1e-9
-itermax = 1000
 
-#initialisation des variables de l'algorithme
+"#initialisation des variables de l'algorithme"
+flag = 0
 xmin = x0
 lambda = lambda0
 mu = mu0
@@ -48,37 +56,39 @@ eta = eta0
 iter = 0
 gradL0 = (gradfonc(x0) + transpose(lambda0)*gradcontrainte(x0) + mu0*transpose(jaccontrainte(x0))*contrainte(x0))
 
-#boucle principale
-while  ((norm(gradfonc(xmin),2)> tol*(norm(gradfonc(x0),2) +epsilon)) || ((normcontrainte(xmin) .> epsilon) && iter <= itermax ) )
+"#boucle principale"
+while  ((norm(gradfonc(xmin),2)> tol*(norm(gradfonc(x0),2) +epsilon)) || ((normcontrainte(xmin) .> epsilon) && iter < itermax ) )
 
-    #la fonction Lagrangien
+    "#la fonction Lagrangien"
     L(x) = (fonc(x) + (transpose(lambda))*contrainte(x) + 0.5*mu*(normcontrainte(x)^2) )
-    #la fonction gradient de Lagrangien
+    "#la fonction gradient de Lagrangien"
     gradL(x) =  (gradfonc(x) + gradcontrainte(x)*lambda + mu*transpose(jaccontrainte(x))*contrainte(x))
-    #la fonction hessienne du Lagrangien
+    "#la fonction hessienne du Lagrangien"
     hessL(x) = (hessfonc(x) + (transpose(lambda))*hesscontrainte(x) + mu*(transpose(jaccontrainte(x)))*jaccontrainte(x) .+ phi(x) )
 
-    #Étape a
-    #Résolution du problème sans contraintes : min L(x,lambdak ,muk)
-    if algorithme_sans_contrainte==0
+    "#Étape a"
+    "#Résolution du problème sans contraintes : min L(x,lambdak ,muk)"
+    if algorithme_sans_contrainte=="NW"
     	xlocal = newton(xmin,gradL,hessL)
-    elseif algorithme_sans_contrainte==1
+    elseif algorithme_sans_contrainte=="RC"
     	xlocal = Regionconf(L,xmin,gradL,hessL,5,1,0.25,0.75,0.5,2,epsk,0)
-    end
 
-    #Test de convergence de l'algorithme global
+    else
+    	err = -1
+    end
+    "#Test de convergence de l'algorithme global"
     if ((norm(gradL(xlocal),2) <= tol*(norm(gradL0,2) +epsilon)) && (normcontrainte(xlocal) <= epsilon))
         xmin = xlocal
         break
 
-    #Mise à jour des variables
+    "#Mise à jour des variables"
     else
-    	#Étape b
+    	"#Étape b"
         if (norm(contrainte(xlocal),2) <= eta)
             lambda = lambda + mu*contrainte(xlocal)
             epsk = epsk/(mu^beta)
             eta = eta /mu
-    	#Étape c
+    	"#Étape c"
         else
             mu = tho*mu
             epsk = eps0/mu
@@ -87,7 +97,10 @@ while  ((norm(gradfonc(xmin),2)> tol*(norm(gradfonc(x0),2) +epsilon)) || ((normc
 
     end
     iter = iter +1
+    if iter==itermax 
+    	flag = 1
+    end
 end
-
-return xmin
+fxmin = fonc(xmin)
+return xmin,fxmin,iter,flag
 end
---
+++ tests/testLagraAug.jl
@@ -0,0 +1,63 @@
+using LinearAlgebra
+include("fonctions_de_tests.jl")
+include("../src/Lagrangien_Augm.jl")
+include("../src/newton.jl")
+
+"#test du lagrangien augmenté"
+
+"#initialisation"
+x01 = [0; 1; 1]
+x02 = [0.5; 1.25; 1]
+x03 = [1; 0]
+x04 = [sqrt(3)/2 ;sqrt(3)/2]
+lambda0 = 2
+mu0 = 100000
+tho = 2
+
+"#paramétres utilisés pour définir les critéres d'arrêts "
+epsilon = 1e-30
+tol = 1e-9
+itermax = 1000
+
+"#Choix d'algorithme d'optimisation sans contraintes"
+algo = "NW"
+
+"#tests "
+xmin1,fxmin1,nbiters,flag = Lagrangien_Augm(algo,fct1,contrainte1,grad_fct1,hess_fct1,grad_contrainte1,hess_contrainte1,norm_contrainte1,jac_contrainte1,phi,x01,epsilon,tol,itermax,lambda0,mu0,tho)
+println("-------------------------------------------------------------------------")
+printstyled("Résultats de l'algorithme du Lagrangien augmenté avec ",algo," appliquée à fct1 et le point initial x01:\n",bold=true,color=:blue)
+println("    * xsol = ",xmin1)
+println("    * f(xsol) = ",fxmin1)
+println("    * nb_iters = ",nbiters)
+println("    * flag = ",flag)
+printstyled("******** Test réussi ******* \n\n",bold=true,color=:green)
+
+xmin2 ,fxmin2,nbiters,flag = Lagrangien_Augm(algo,fct1,contrainte1,grad_fct1,hess_fct1,grad_contrainte1,hess_contrainte1,norm_contrainte1,jac_contrainte1,phi,x02,epsilon,tol,itermax,lambda0,mu0,tho)
+println("-------------------------------------------------------------------------")
+printstyled("Résultats de l'algorithme du Lagrangien augmenté avec ",algo," appliquée à fct1 et le point initial x02:\n",bold=true,color=:blue)
+println("    * xsol = ",xmin2)
+println("    * f(xsol) = ",fxmin2)
+println("    * nb_iters = ",nbiters)
+println("    * flag = ",flag)
+printstyled("******** Test réussi ******* \n\n",bold=true,color=:green)
+
+
+xmin3,fxmin3,nbiters,flag = Lagrangien_Augm(algo,fct2,contrainte2,grad_fct2,hess_fct2,grad_contrainte2,hess_contrainte2,norm_contrainte2,jac_contrainte2,phi,x03,epsilon,tol,itermax,lambda0,mu0,tho)
+println("-------------------------------------------------------------------------")
+printstyled("Résultats de l'algorithme du Lagrangien augmenté avec ",algo," appliquée à fct1 et le point initial x03\n:",bold=true,color=:blue)
+println("    * xsol = ",xmin3)
+println("    * f(xsol) = ",fxmin3)
+println("    * nb_iters = ",nbiters)
+println("    * flag = ",flag)
+printstyled("******** Test réussi ******* \n\n",bold=true,color=:green)
+
+
+xmin4 ,fxmin4,nbiters,flag = Lagrangien_Augm(algo,fct2,contrainte2,grad_fct2,hess_fct2,grad_contrainte2,hess_contrainte2,norm_contrainte2,jac_contrainte2,phi,x04,epsilon,tol,itermax,lambda0,mu0,tho)
+println("-------------------------------------------------------------------------")
+printstyled("Résultats de l'algorithme du Lagrangien augmenté avec "*algo*" appliquée à fct1 et le point initial x04:\n",bold=true,color=:blue)
+println("    * xsol = ",xmin4)
+println("    * f(xsol) = ",fxmin4)
+println("    * nb_iters = ",nbiters)
+println("    * flag = ",flag)
+printstyled("******** Test réussi ******* \n\n",bold=true,color=:green)
+
