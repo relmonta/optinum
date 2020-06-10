4af6b20a4f216970d40eae22db1bb5e2009ffd73
relmo <elmontassir.rachid1937@gmail.com>
2020-06-10 19:21:57 +0200

    add package Optinum

---
+++ src/Optinum.jl
@@ -0,0 +1,17 @@
+module Optinum
+
+using LinearAlgebra
+using Test
+using JuMP
+using Ipopt
+
+include("Algorithme_de_Newton.jl")
+include("Gradient_Conjugue_Tronque.jl")
+include("Lagrangien_Augmente.jl")
+include("Optinum.jl")
+include("Pas_De_Cauchy.jl")
+include("Regions_de_confiance.jl")
+
+export  Algorithme_de_Newton, Gradient_Conjugue_Tronque, Lagrangien_Augmente, Pas_De_Cauchy, Regions_de_confiance
+
+end # module
